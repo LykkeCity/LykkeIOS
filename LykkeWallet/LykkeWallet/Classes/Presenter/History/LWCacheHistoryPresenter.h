16d54331ee1cda3d9beab5695daa1886f07bfa6d
Alexander Pukhov <siquell@lifeportal.ru>
2016-03-15 23:45:49 +0300

    - IPHONELW-132

---
+++ LykkeWallet/LykkeWallet/Classes/Presenter/History/LWCacheHistoryPresenter.h
@@ -0,0 +1,13 @@
+//
+//  LWCacheHistoryPresenter.h
+//  LykkeWallet
+//
+//  Created by Alexander Pukhov on 15.03.16.
+//  Copyright © 2016 Lykkex. All rights reserved.
+//
+
+#import "LWAuthenticatedTablePresenter.h"
+
+@interface LWCacheHistoryPresenter : LWAuthenticatedTablePresenter
+
+@end
