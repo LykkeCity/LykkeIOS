229242b6dfc0a5d6a32860ccb5b80dfa84c84089
Alexander Pukhov <siquell@lifeportal.ru>
2016-03-31 18:44:25 +0300

    - IPHONELW-151 (confirmation view)

---
+++ LykkeWallet/LykkeWallet/Classes/Model/Core/Network/LWPacketCashOut.h
@@ -0,0 +1,13 @@
+//
+//  LWPacketCashOut.h
+//  LykkeWallet
+//
+//  Created by Alexander Pukhov on 31.03.16.
+//  Copyright © 2016 Lykkex. All rights reserved.
+//
+
+#import "LWAuthorizePacket.h"
+
+@interface LWPacketCashOut : LWAuthorizePacket
+
+@end
---
+++ LykkeWallet/LykkeWallet/Classes/Model/Core/Network/LWPacketCashOut.m
@@ -0,0 +1,13 @@
+//
+//  LWPacketCashOut.m
+//  LykkeWallet
+//
+//  Created by Alexander Pukhov on 31.03.16.
+//  Copyright © 2016 Lykkex. All rights reserved.
+//
+
+#import "LWPacketCashOut.h"
+
+@implementation LWPacketCashOut
+
+@end
