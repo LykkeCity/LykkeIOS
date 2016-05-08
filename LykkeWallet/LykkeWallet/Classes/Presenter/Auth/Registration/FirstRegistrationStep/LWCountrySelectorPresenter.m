87df874cfe98a05961bb6d2c0438d6f07f812443
Alexander Pukhov <siquell@lifeportal.ru>
2016-05-08 13:46:04 +0300

    - IPHONELW-193

--- LykkeWallet/LykkeWallet/Classes/Presenter/Auth/Registration/FirstRegistrationStep/LWCountrySelectorPresenter.m
+++ LykkeWallet/LykkeWallet/Classes/Presenter/Auth/Registration/FirstRegistrationStep/LWCountrySelectorPresenter.m
@@ -15,14 +15,13 @@
 #import "UIViewController+Loading.h"
 
 
-@interface LWCountrySelectorPresenter () <UISearchBarDelegate> {
+@interface LWCountrySelectorPresenter () <UISearchBarDelegate, UISearchResultsUpdating, UISearchControllerDelegate> {
     NSArray *countries;
+    NSArray *searchCountries;
     NSArray *sections;
 }
 
-@property (nonatomic, strong) NSString *filter;
-
-@property (nonatomic, weak) IBOutlet UISearchBar *searchBar;
+@property (nonatomic, strong) UISearchController *searchController;
 
 
 #pragma mark - Utils
@@ -43,22 +42,34 @@
     self.navigationItem.title = Localize(@"register.phone.country.title");
     
     countries = [NSArray array];
-    sections = [NSArray arrayWithObjects:@"#", @"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z", nil];
+    sections = [NSArray arrayWithObjects:@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z", nil];
     
     [self setCancelButtonWithTitle:Localize(@"register.phone.cancel")
                             target:self
                           selector:@selector(cancelClicked)];
- 
+    
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.searchResultsUpdater = self;
+    self.searchController.delegate = self;
+    self.searchController.searchBar.delegate = self;
+    self.searchController.dimsBackgroundDuringPresentation = NO;
+    
     NSString *searchText = Localize(@"register.phone.country.placeholder");
-    [self.searchBar setKeyboardType:UIKeyboardTypeDefault];
-    [self.searchBar setPlaceholder:searchText];
-    [self.searchBar setBarStyle:UIBarStyleDefault];
-    [self.searchBar setAutocorrectionType:UITextAutocorrectionTypeNo];
+    [self.searchController.searchBar setKeyboardType:UIKeyboardTypeDefault];
+    [self.searchController.searchBar setPlaceholder:searchText];
+    [self.searchController.searchBar setBarStyle:UIBarStyleDefault];
+    [self.searchController.searchBar setAutocorrectionType:UITextAutocorrectionTypeNo];
+    
+    [self.searchController.searchBar setBackgroundImage:[UIImage new]];
+    [self.searchController.searchBar sizeToFit];
     
     [self registerCellWithIdentifier:kCountryTableViewCellIdentifier
                              forName:kCountryTableViewCell];
     
     self.tableView.sectionIndexColor = [UIColor colorWithHexString:kMainElementsColor];
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+    
+    self.navigationController.navigationBar.translucent = NO;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -66,11 +77,30 @@
     
     [self setLoading:YES];
     
+    //self.definesPresentationContext = YES;
+    //self.extendedLayoutIncludesOpaqueBars = YES;
+    //self.edgesForExtendedLayout = UIRectEdgeNone;
+    //self.tableView.contentOffset = CGPointMake(0, 44.0);
+    //self.tableView.contentInset = UIEdgeInsetsMake(44.0, 0.0, 0.0, 0.0);
+    
     [[LWAuthManager instance] requestCountyCodes];
 }
 
+- (void)dealloc {
+    [self.searchController.view removeFromSuperview];
+}
+
 - (void)cancelClicked {
+    [self.searchController setActive:NO];
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)willPresentSearchController:(UISearchController *)searchController {
+    self.navigationController.navigationBar.translucent = YES;
+}
+
+- (void)didDismissSearchController:(UISearchController *)searchController {
+    self.navigationController.navigationBar.translucent = NO;
 }
 
 
@@ -85,42 +115,61 @@
 - (void)authManager:(LWAuthManager *)manager didFailWithReject:(NSDictionary *)reject context:(GDXRESTContext *)context {
     [self showReject:reject response:context.task.response code:context.error.code willNotify:YES];
     
-    [self dismissViewControllerAnimated:YES completion:nil];
+    [self cancelClicked];
 }
 
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    if (self.searchController.active) {
+        return 1;
+    }
     return sections.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-
-    NSArray *items = [self countriesBySection:section];
-    NSInteger result = items.count;
+    NSInteger result = 0;
+    if (self.searchController.active) {
+        result = searchCountries.count;
+    }
+    else {
+        NSArray *items = [self countriesBySection:section];
+        result = items.count;
+    }
     return result;
 }
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
 {
+    if (self.searchController.active) {
+        return nil;
+    }
     return sections;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString*)title atIndex:(NSInteger)index
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
 {
+    if (self.searchController.active) {
+        return 0;
+    }
     return index;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    NSString *sectionHeader = [NSString stringWithFormat:@"  %@", [sections objectAtIndex:section]];
+    if (self.searchController.active) {
+        return @"";
+    }
+    
+    NSString *sectionHeader = [NSString stringWithFormat:@"  %@",
+                               [sections objectAtIndex:section]];
     return sectionHeader;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCountryTableViewCellIdentifier];
-    [self configureCell:cell indexPath:indexPath];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:kCountryTableViewCellIdentifier];
+    [self configureCell:cell tableView:tableView indexPath:indexPath];
     
     return cell;
 }
@@ -132,18 +181,24 @@
                                   code:cell.model.identity
                                 prefix:cell.model.prefix];
 
-        [self dismissViewControllerAnimated:YES completion:nil];
+        [self cancelClicked];
     }
 }
 
 
 #pragma mark - LWAuthenticatedTablePresenter
 
-- (void)configureCell:(UITableViewCell *)cell indexPath:(NSIndexPath *)indexPath {
+- (void)configureCell:(UITableViewCell *)cell tableView:(UITableView *)tableView indexPath:(NSIndexPath *)indexPath {
     LWCountryTableViewCell *itemCell = (LWCountryTableViewCell *)cell;
-    NSArray *items = [self countriesBySection:indexPath.section];
-    LWCountryModel *model = (LWCountryModel *)[items objectAtIndex:indexPath.row];
-    
+    NSArray *items = nil;
+    if (self.searchController.active) {
+        items = searchCountries;
+    }
+    else {
+        items = [self countriesBySection:indexPath.section];
+    }
+
+    LWCountryModel *model = (LWCountryModel *)[items objectAtIndex:indexPath.row];;
     if (model && itemCell) {
         itemCell.countryLabel.text = model.name;
         itemCell.codeLabel.text = model.prefix;
@@ -151,38 +206,31 @@
     }
 }
 
-/*
+
+#pragma mark - UISearchResultsUpdating
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
+{
+    NSString *searchString = searchController.searchBar.text;
+    [self searchForText:searchString];
+    [self.tableView reloadData];
+}
+
+
 #pragma mark - Searching
 
-- (void)filterContentForSearchText:(NSString *)searchText scope:(NSString *)scope
+- (void)searchForText:(NSString *)searchText
 {
-    [self setFilter:searchText];
-    //[self updateSelectedIndexPath];
+    if (searchText == nil ||
+        [searchText isKindOfClass:[NSNull class]] ||
+        searchText.length <= 0) {
+        searchCountries = countries;
+    }
+    else {
+        NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"name beginswith[c] %@", searchText];
+        searchCountries = [countries filteredArrayUsingPredicate:resultPredicate];
+    }
 }
-
-#pragma mark - UISearchDisplayController Delegate Methods
-
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
-{
-    // Tells the table data source to reload when text changes
-    [self filterContentForSearchText:searchString scope: [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
-    // Return YES to cause the search result table view to be reloaded.
-    return YES;
-}
-
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
-{
-    // Tells the table data source to reload when scope bar selection changes
-    [self filterContentForSearchText:self.searchDisplayController.searchBar.text scope:
-     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:searchOption]];
-    // Return YES to cause the search result table view to be reloaded.
-    return YES;
-}
-
-- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar
-{
-    [self filterContentForSearchText:nil scope:nil];
-}*/
 
 
 #pragma mark - Utils
